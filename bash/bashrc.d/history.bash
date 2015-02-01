a07766515b3e215685ed01e085ca6b7fca02a2d2
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2015-02-01 12:17:26 -0600

    Split history control into a bash subfile

---
+++ bash/bashrc.d/history.bash
@@ -0,0 +1,18 @@
+# ignore duplicate entires, and ignore commands that start with space
+HISTCONTROL=ignoredups:ignorespace
+
+# append to the history file, don't overwrite it
+shopt -s histappend &>/dev/null
+
+# put multi-line commands onto one line of history.
+shopt -s cmdhist &>/dev/null
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+unset HISTFILESIZE
+HISTSIZE=1000000
+
+# keep the times of the commands in history
+HISTTIMEFORMAT='%F %T  '
+
+# add history entries immediately, not on exit
+PROMPT_COMMAND='history -a'
