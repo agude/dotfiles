5114c84b736f2150ae560c2ed5459ffeaeab2b30
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2015-10-17 17:39:53 -0700

    Add tnice.sh
    tnice.sh runs a command using both `nice` and `ionice`.

---
+++ bin/tnice.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Exit if any errors or if any needed variables are unset
+set -e
+set -u
+
+# Wrap the command in nice
+nice -n 19 ionice -c 2 -n 7 "$@"
+
+# Return exit code
+exit $?
