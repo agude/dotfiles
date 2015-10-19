be0fb785742bc948b69886ea76220d28f0fb5497
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2015-10-18 20:51:32 -0700

    Add rmspace
    rmspace.sh removes spaces in files names in a directory.

---
+++ bin/rmspace.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit if any errors or if any needed variables are unset
+set -e
+set -u
+
+# Replace every file, but only if it doesn't already conform to our naming
+# scheme, and if it would not overwrite an already existent file
+for file in *; do
+    # s/  */_/g replaces spaces with _
+    # s/_-_/-/g replaces _-_ with -
+    new_name=$(echo ${file} | sed -e 's/  */_/g' -e 's/_-_/-/g')
+    # Only move if it doesn't already exist, and isn't already the right name
+    if [[ ! -f ${new_name} && "${file}" != "${new_name}" ]]; then
+        mv "${file}" ${new_name}
+    fi
+done
+
+# Return exit code
+exit $?
