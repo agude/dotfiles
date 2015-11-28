31084c3c11873d69ec1089f77e884645bee6743c
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2015-11-27 21:31:05 -0800

    Move libice .ICEauthority file to $XDG_RUNTIME_DIR

--- bash/bashrc.d/xdg_base_directory.bash
+++ bash/bashrc.d/xdg_base_directory.bash
@@ -16,3 +16,7 @@ INPUTRC=${XDG_CONFIG_HOME}/readline/inputrc
 export IPYTHONDIR="${XDG_CONFIG_HOME}"/jupyter
 export JUPYTER_CONFIG_DIR="${XDG_CONFIG_HOME}"/jupyter
 mkdir -p ${IPYTHONDIR} ${JUPYTER_CONFIG_DIR}
+
+## libice
+export ICEAUTHORITY="${XDG_RUNTIME_DIR}"/X11/iceauthority
+mkdir -p $(dirname ${ICEAUTHORITY})
