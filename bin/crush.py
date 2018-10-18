812e3b0e7a0b9fbf1f04e34e4a1b0aaf319503ff
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2018-10-17 21:42:39 -0700

    Add argparse to `bin/crush.py`
    Also simplify the code some.
    
    Fixes #1.

--- bin/crush.py
+++ bin/crush.py
@@ -14,50 +14,56 @@ Exit Codes:
     2: pngout executable not found on the user's path.
 """
 
-from subprocess import call  # Access external programs
-from sys import argv
-from os.path import isfile
-
-# Multiprocessing does not exist in some older versions of python
-HasMP = True
-NJOBS = 1
-try:
-    import multiprocessing as mp
-except ImportError:
-    HasMP = False
-else:  # Only runs when try succeeds
-    from math import floor
-    if HasMP:
-        NJOBS = int(floor(mp.cpu_count() * 1.5))
-
-# Try to find pngout
 from distutils.spawn import find_executable
-if find_executable("pngout") is None:
-    print("Can not find pngout.")
-    exit(2)
+from math import floor
+from subprocess import call
+import argparse
 
-# Input files, 1: slicing removes this programs name from the list
-inputfiles = [f for f in argv if isfile(f)][1:]
 
-# List to store the commands to run over before passing to multiprocessing
-commands = []
-for f in inputfiles:
-    command = (
+if __name__ == "__main__":
+    # Parse the list of files
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "files",
+        nargs="+",
+        help="one or more files to process with pngcrush",
+    )
+    args = parser.parse_args()
+
+    # Try to find pngout
+    if find_executable("pngout") is None:
+        print("Can not find pngout.")
+        exit(2)
+
+    # Get the number of jobs to run
+    NJOBS = 1
+    try:
+        import multiprocessing as mp
+    except ImportError:
+        NJOBS = 1
+    else:  # Only runs when try succeeds
+        floor_j = int(floor(mp.cpu_count() * 1.5))
+        NJOBS = max(floor_j, 1)
+
+    # List to store the commands to run over before passing to multiprocessing
+    commands = []
+    for f in args.files:
+        command = (
             #"nice", "-n", "19",  # Limit processor prio
             #"ionice", "-c", "2", "-n", "7",  # Limit IO prio
             "pngout", "-y", f  # Run the compression, overwrite if asked
-            )
-    commands.append(command)
+        )
+        commands.append(command)
 
-# Run jobs in parallel
-if HasMP and NJOBS > 1:
-    pool = mp.Pool(processes=NJOBS)
-    pool.map(call, commands)  # No return values so we don't care about them
-    pool.close()  # No more tasks to add
-    pool.join()  # Wait for jobs to finish
-else:
-    for com in commands:
-        call(com)
+    # Run jobs in parallel
+    if NJOBS > 1:
+        pool = mp.Pool(processes=NJOBS)
+        pool.map(call, commands)  # No return values so we don't care about them
+        pool.close()  # No more tasks to add
+        pool.join()  # Wait for jobs to finish
+    else:
+        for com in commands:
+            call(com)
 
-# Exit ok
-exit(0)
+    # Exit ok
+    exit(0)
