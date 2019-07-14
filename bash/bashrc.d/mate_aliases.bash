5fb2102e65a1f889f8ed6b4af1e4d4f2687affba
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2019-07-14 13:10:11 -0700

    Add MATE aliases

---
+++ bash/bashrc.d/mate_aliases.bash
@@ -0,0 +1,15 @@
+set_alias_if_program_exists() {
+    PROGRAM=$1
+    ALIAS_NAME=$2
+
+    # Check if the command exists, and if so aliases
+    if [ -x "$(command -v ${PROGRAM})" ]; then
+        alias ${ALIAS_NAME}="${PROGRAM}"
+    fi
+}
+
+# Set aliases for the renamed MATE applications
+set_alias_if_program_exists 'atril' 'evince'
+set_alias_if_program_exists 'caja' 'nautilus'
+set_alias_if_program_exists 'eom' 'eog'
+set_alias_if_program_exists 'pluma' 'gedit' 
