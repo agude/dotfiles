a4870cba83be9df5955b75742adc9c5e1a4f1267
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2021-11-24 09:51:31 -0800

    Change from Anaconda to pyenv

---
+++ bash/bashrc.d/201.python.bash
@@ -0,0 +1,9 @@
+# Add Pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi
+
+# Add virtualenv
+eval "$(pyenv virtualenv-init -)"
