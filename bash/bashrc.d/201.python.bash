f982dfdcc00aa1aecd910e767c3530ba8510d35e
Alexander Gude <alex.public.account@gmail.com>
2022-05-06 19:23:40 -0400

    Update pyenv script for my Mac

--- bash/bashrc.d/201.python.bash
+++ bash/bashrc.d/201.python.bash
@@ -2,8 +2,11 @@
 export PYENV_ROOT="${HOME}/.pyenv"
 export PATH="${PYENV_ROOT}/bin:${PATH}"
 if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
 fi
 
 # Add virtualenv
-eval "$(pyenv virtualenv-init -)"
+if which pyenv-virtualenv-init > /dev/null; then
+    eval "$(pyenv virtualenv-init -)"
+fi
