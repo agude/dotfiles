89eaf29d0eee0ae55960c9f8e5b2c7582a19c94b
Alexander Gude <alex.public.account@gmail.com>
2022-05-06 21:47:08 -0400

    Add support for gdircolors on MacOS

--- bash/bashrc.d/102.dircolors.bash
+++ bash/bashrc.d/102.dircolors.bash
@@ -1,4 +1,7 @@
 # Set directory and file colors in bash
 if [[ -x $(command -v dircolors) ]]; then
     eval "$(dircolors -b)"
+# gdircolors is the version installed by homebrew on MacOS
+elif [[ -x $(command -v gdircolors) ]]; then
+    eval "$(gdircolors -b)"
 fi
