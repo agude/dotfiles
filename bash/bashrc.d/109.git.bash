a34582f15e767a982761eff32adf140889ca72be
Alexander Gude <alex.public.account@gmail.com>
2022-05-06 20:03:12 -0400

    Source git-completion.bash
    But only the most recent version

---
+++ bash/bashrc.d/109.git.bash
@@ -0,0 +1,9 @@
+if [[ -x $(command -v brew) ]]; then
+    SCRIPT_FILE="$(brew --prefix)"/etc/bash_completion.d/git-completion.bash
+
+    if [[ -f ${SCRIPT_FILE} ]]; then
+        source ${SCRIPT_FILE}
+    fi
+
+    unset SCRIPT_FILE
+fi
