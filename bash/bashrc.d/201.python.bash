5d4b9538e3d04534298a8a4fff5ea03fc8a3ce26
Alexander Gude <alex.public.account+dotfiles@gmail.com>
2023-10-21 13:49:26 -0700

    Fix Pyenv

--- bash/bashrc.d/201.python.bash
+++ bash/bashrc.d/201.python.bash
@@ -1,12 +1,10 @@
 # Add Pyenv
 export PYENV_ROOT="${HOME}/.pyenv"
 export PATH="${PYENV_ROOT}/bin:${PATH}"
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init --path)"
+if command -v pyenv >/dev/null; then
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
     eval "$(pyenv init -)"
 fi
 
 # Add virtualenv
-if which pyenv-virtualenv-init > /dev/null; then
-    eval "$(pyenv virtualenv-init -)"
-fi
+eval "$(pyenv virtualenv-init -)"
