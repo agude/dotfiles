fe0d646308a698e6767b047ec70335daa06edc07
Alexander Gude <alex.public.account@gmail.com>
2025-03-22 19:45:55 -0700

    Remove pyenvs and update spell

--- bash/bashrc.d/201.python.bash
+++ bash/bashrc.d/201.python.bash
@@ -1,10 +1,10 @@
 # Add Pyenv
-export PYENV_ROOT="${HOME}/.pyenv"
-export PATH="${PYENV_ROOT}/bin:${PATH}"
-if command -v pyenv >/dev/null; then
-    export PATH="${PYENV_ROOT}/bin:${PATH}"
-    eval "$(pyenv init -)"
-fi
-
-# Add virtualenv
-eval "$(pyenv virtualenv-init -)"
+#export PYENV_ROOT="${HOME}/.pyenv"
+#export PATH="${PYENV_ROOT}/bin:${PATH}"
+#if command -v pyenv >/dev/null; then
+#    export PATH="${PYENV_ROOT}/bin:${PATH}"
+#    eval "$(pyenv init -)"
+#fi
+#
+## Add virtualenv
+#eval "$(pyenv virtualenv-init -)"
