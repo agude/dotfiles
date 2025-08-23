0c8c1ee59db8a1af3f58e8466da2c3a883c5df36
Alexander Gude <alex.public.account@gmail.com>
2025-08-22 19:06:07 -0700

    Update prompt

--- zsh/zshrc.d/107.prompt.zsh
+++ zsh/zshrc.d/107.prompt.zsh
@@ -1,79 +1,57 @@
-# Functions to generate prompt segments
+# ------------------------------------------------------------------------------
+# Zsh Prompt Configuration
+# ------------------------------------------------------------------------------
 
-# Sets ZSH to do parameter expansion, command substitution and arithmetic
-# expansion in prompts
+# Allow for prompt expansion and substitution.
 setopt prompt_subst
 
-# return status
-prompt_return_status() {
-    # Display the last exit code but only if non-0
-    echo -n "%(?..%F{red}exit: %?%f\n)"
-}
-
-# username
-prompt_username() {
-  if [[ $EUID -eq 0 ]]; then
-    color='red'
-  elif [[ -n $SUDO_USER ]]; then
-    color='yellow'
-  elif [[ -n $SSH_CLIENT ]]; then
-    color='magenta'
-  else
-    color='green'
-  fi
-
-  echo -n "%F{$color}%n%f"
-}
-
-# directory
-prompt_dir() {
-  echo -n '%F{blue}%~%f'
-}
-
-# pyenv
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-prompt_pyenv() {
-  if [[ -n "$PYENV_VERSION" ]]; then
-    echo -n "($PYENV_VERSION) "
-  fi
-}
-
-# git
-
-# From: https://salferrarello.com/zsh-git-status-prompt/
+# --- VCS (Git) Prompt Setup ---
 autoload -Uz vcs_info
-
-# Enable checking for (un)staged changes, enabling use of %u and %c
+zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
-# Set custom strings for an unstaged vcs repo changes (*) and staged changes (+)
 zstyle ':vcs_info:*' unstagedstr ' *'
 zstyle ':vcs_info:*' stagedstr ' +'
-# Set the format of the Git information for vcs_info
-zstyle ':vcs_info:git:*' formats       '(%b%u%c)'
-zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c)'
+# Set the format and add a space at the beginning for padding
+zstyle ':vcs_info:git:*' formats       ' %F{red}(%b%u%c)%f'
+zstyle ':vcs_info:git:*' actionformats ' %F{red}(%b|%a%u%c)%f'
 
-prompt_git() {
-  vcs_info
-  if [[ -n $vcs_info_msg_0_ ]]; then
-    echo -n " %F{red}${vcs_info_msg_0_}%f"
+# --- Pyenv Prompt Setup ---
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+prompt_pyenv_segment() {
+  # This function populates the global $pyenv_prompt_segment variable
+  if [[ -n "$PYENV_VERSION" ]]; then
+    pyenv_prompt_segment="($PYENV_VERSION) "
+  else
+    pyenv_prompt_segment=""
   fi
 }
 
-# Build prompt
-build_prompt() {
-    local prompt
-    prompt+=$(prompt_return_status)
-    prompt+=$(prompt_pyenv)
-    prompt+=$(prompt_username)
-    prompt+=':'
-    prompt+=$(prompt_dir)
-    prompt+=$(prompt_git)
-    prompt+='$ '
-
-    echo -n $prompt
-}
-
-# Set prompt
+# --- precmd: Runs before each prompt is displayed ---
 precmd() {
-  PROMPT=$(build_prompt)
+  # Update git status. The result is stored in $vcs_info_msg_0_.
+  vcs_info
+
+  # Update pyenv status.
+  prompt_pyenv_segment
+
+  # --- Build the PROMPT variable ---
+
+  # 1. Exit Code (only shown on error)
+  PROMPT='%(?..%F{red}exit: %?%f )'
+
+  # 2. Pyenv Segment
+  PROMPT+="${pyenv_prompt_segment}"
+
+  # 3. Username (green for normal, yellow for sudo, red for root)
+  PROMPT+='%F{%(?.green.%(?.yellow.red))}%n%f'
+
+  # 4. Directory
+  PROMPT+=':%F{blue}%~%f'
+
+  # 5. Git Segment
+  PROMPT+="${vcs_info_msg_0_}"
+
+  # 6. Final prompt symbol
+  PROMPT+=' ' # Use a space instead of '$ ' to avoid a double space if there's no git info
+  PROMPT+='%(!.#.$) ' # Show '#' for root, '$' for normal users
 }
