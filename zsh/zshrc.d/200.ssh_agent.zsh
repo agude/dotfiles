a87b9b9f3e5b9e30bfbb3cd1dc6191769d73d31e
Alexander Gude <alex.public.account@gmail.com>
2025-08-30 09:59:54 -0700

    Configure zsh to use systemd ssh-agent

--- zsh/zshrc.d/200.ssh_agent.zsh
+++ zsh/zshrc.d/200.ssh_agent.zsh
@@ -1,2 +1,2 @@
-# Point to the ssh-agent socket managed by systemd
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+# Find and use the systemd-managed ssh-agent socket
+export SSH_AUTH_SOCK=$(systemctl --user show-environment | grep -oP 'SSH_AUTH_SOCK=\K.*')
