0f934cb4b4ce3c3033c678251b20a2da90745936
Alexander Gude <alex.public.account@gmail.com>
2026-01-29 17:37:03 -0800

    Fix shellcheck SC2155 in man page color config
    Separate declaration and assignment of LESS_TERMCAP_* variables so
    tput failures are not masked by the export return value.

--- bash/bashrc.d/106.man.bash
+++ bash/bashrc.d/106.man.bash
@@ -15,14 +15,23 @@ _blue=4
 _magenta=5
 _white=7
 
-export LESS_TERMCAP_mb=$(tput bold; tput ${_text} ${_red})              # Start blinking
-export LESS_TERMCAP_md=$(tput bold; tput ${_text} ${_blue})             # Start bold mode
-export LESS_TERMCAP_mh=$(tput dim)                                      # Start half bright mode
-export LESS_TERMCAP_mr=$(tput rev)                                      # Start reverse mode
-export LESS_TERMCAP_so=$(tput ${_bg} ${_white}; tput ${_text} ${_black}) # Start standout mode
-export LESS_TERMCAP_se=$(tput rmso; tput sgr0)                          # End standout mode
-export LESS_TERMCAP_us=$(tput smul; tput bold; tput ${_text} ${_magenta}) # Start underlining
-export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)                          # End underlining
-export LESS_TERMCAP_me=$(tput sgr0)                                     # End all modes
+LESS_TERMCAP_mb=$(tput bold; tput ${_text} ${_red})                # Start blinking
+export LESS_TERMCAP_mb
+LESS_TERMCAP_md=$(tput bold; tput ${_text} ${_blue})               # Start bold mode
+export LESS_TERMCAP_md
+LESS_TERMCAP_mh=$(tput dim)                                        # Start half bright mode
+export LESS_TERMCAP_mh
+LESS_TERMCAP_mr=$(tput rev)                                        # Start reverse mode
+export LESS_TERMCAP_mr
+LESS_TERMCAP_so=$(tput ${_bg} ${_white}; tput ${_text} ${_black})  # Start standout mode
+export LESS_TERMCAP_so
+LESS_TERMCAP_se=$(tput rmso; tput sgr0)                            # End standout mode
+export LESS_TERMCAP_se
+LESS_TERMCAP_us=$(tput smul; tput bold; tput ${_text} ${_magenta}) # Start underlining
+export LESS_TERMCAP_us
+LESS_TERMCAP_ue=$(tput rmul; tput sgr0)                            # End underlining
+export LESS_TERMCAP_ue
+LESS_TERMCAP_me=$(tput sgr0)                                       # End all modes
+export LESS_TERMCAP_me
 
 unset -v _text _bg _black _red _blue _magenta _white
